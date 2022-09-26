de189a7e25d1438b6cd2509aced0d3590354fe38
lodity <lodity9@gmail.com>
2022-09-26 18:10:07 +0300

    async action creator

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { fetchUsers } from '../store/action-creators/user';
 
 const UserList: React.FC = () => {
 	const { users, loading, error } = useTypedSelector((state) => state.user);
-	return <div></div>;
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, []);
+
+	if (loading) {
+		return <h1>Loading...</h1>;
+	}
+	if (error) {
+		return <h1>Error!</h1>;
+	}
+
+	return (
+		<div>
+			{users.map((user) => (
+				<div key={user.id}>{user.name}</div>
+			))}
+		</div>
+	);
 };
 
 export default UserList;
