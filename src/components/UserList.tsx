48c8e6de7ec77cc877be3aa5865ee29cc364812a
lodity <lodity9@gmail.com>
2022-09-26 16:29:53 +0300

    new hook useTypedSelector

---
+++ src/components/UserList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
+const UserList: React.FC = () => {
+	const { users, loading, error } = useTypedSelector((state) => state.user);
+	return <div></div>;
+};
+
+export default UserList;
